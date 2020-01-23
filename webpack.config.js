405e0b0dfda1a28aa97bd1bcda0a7de62d178eb2
Joshua Loehr <loehrj@students.wwu.edu>
2020-01-23 15:49:29 -0800

    Change webpack to use index.html template

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'My Application'
+            template: './src/index.html'
         })
     ],
     module: {
