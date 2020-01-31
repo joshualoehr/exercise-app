b1860462f3f6b3f2d56faeb8f19643a2040bfb9d
Joshua Loehr <loehrj@students.wwu.edu>
2020-01-30 21:06:57 -0800

    Remove unnecessary import

--- src/features/common/Frame.js
+++ src/features/common/Frame.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
